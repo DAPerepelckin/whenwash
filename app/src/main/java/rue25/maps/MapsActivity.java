589ab60f13eca2bd54352c4dd3501a27ff9aca6d
DAPerepelckin <perep.dima@gmail.com>
2018-04-14 22:50:51 +0300

    123

--- app/src/main/java/rue25/maps/MapsActivity.java
+++ app/src/main/java/rue25/maps/MapsActivity.java
@@ -12,7 +12,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
