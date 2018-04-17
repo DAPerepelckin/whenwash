8b220646c8adae642ff666b0526cd00ddfc38d0b
DAPerepelckin <perep.dima@gmail.com>
2018-04-17 19:19:58 +0300

    xkty

--- app/src/main/java/com/example/dap/whenwash/MainActivity.java
+++ app/src/main/java/com/example/dap/whenwash/MainActivity.java
@@ -1,16 +1,9 @@
 package com.example.dap.whenwash;
 
-import android.content.Context;
-import android.content.DialogInterface;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -20,14 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import rue25.maps.MapsActivity;
 import rue25.maps.MapsActivity1;
@@ -40,8 +25,7 @@ public class MainActivity extends AppCompatActivity
     public static final String APP_PREFERENCES = "auth";
     public static final String APP_PREFERENCES_EMAIL = "Email";
     public static final String APP_PREFERENCES_PASSWORD = "Password";
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
+
     private String email="";
     private String password1="";
 
@@ -51,24 +35,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LoadData();
-
-        mAuth = FirebaseAuth.getInstance();
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-
-                } else {
-
-
-                }
-                // ...
-            }
-        };
-
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -110,112 +76,19 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
-    public void dial(){
 
-        LayoutInflater li = LayoutInflater.from(this);
-        View promptsView = li.inflate(R.layout.auth, null);
-        AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(this);
-        mDialogBuilder.setView(promptsView);
-        final EditText userInput = (EditText) promptsView.findViewById(R.id.input_text);
-        final EditText userInput1 = (EditText) promptsView.findViewById(R.id.input_text1);
-
-
-        mAuth = FirebaseAuth.getInstance();
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-
-                } else {
-
-
-                }
-                // ...
-            }
-        };
-
-
-
-        mDialogBuilder
-                .setCancelable(false)
-                .setPositiveButton("OK",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(final DialogInterface dialog, int id) {
-                                email = userInput.getText().toString();
-                                password1 = userInput1.getText().toString();
-                                if(email.isEmpty()||password1.isEmpty()) {
-                                    dialog.dismiss();
-                                    dial(); }else {
-
-
-                                    mAuth.signInWithEmailAndPassword(email, password1)
-                                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                                    SaveData();
-                                                    // If sign in fails, display a message to the user. If sign in succeeds
-                                                    // the auth state listener will be notified and logic to handle the
-                                                    // signed in user can be handled in the listener.
-                                                    if (!task.isSuccessful()) {
-                                                        dialog.dismiss();
-                                                        dial();
-                                                        Toast.makeText(MainActivity.this, R.string.auth_failed,
-                                                                Toast.LENGTH_SHORT).show();
-                                                    }
-
-                                                    // ...
-                                                }
-                                            });
-
-
-                                } }}).setNeutralButton("Добавить",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(final DialogInterface dialog, int id) {
-                        email = userInput.getText().toString();
-                        password1 = userInput1.getText().toString();
-                        if (email.isEmpty() || password1.isEmpty()) {
-                            dialog.dismiss();
-                            dial();
-                        } else {
-
-
-                            mAuth.createUserWithEmailAndPassword(email, password1)
-                                    .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<AuthResult> task) {
-                                            SaveData();
-                                            // If sign in fails, display a message to the user. If sign in succeeds
-                                            // the auth state listener will be notified and logic to handle the
-                                            // signed in user can be handled in the listener.
-                                            if (!task.isSuccessful()) {
-                                                dialog.dismiss();
-                                                dial();
-                                                Toast.makeText(MainActivity.this, R.string.auth_failed,
-                                                        Toast.LENGTH_SHORT).show();
-                                            }
-
-                                            // ...
-                                        }
-                                    });
-                        }
-                    } }
-        );
-    }
     @Override
     public void onStart() {
         super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
+       // mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
+       // if (mAuthListener != null) {
+         //   mAuth.removeAuthStateListener(mAuthListener);
+        //}
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -271,43 +144,9 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onClick(View view) {
-            if(email.isEmpty()||password1.isEmpty()){
-            dial();}else{
-            mAuth = FirebaseAuth.getInstance();
-
-            mAuthListener = new FirebaseAuth.AuthStateListener() {
-                @Override
-                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                    FirebaseUser user = firebaseAuth.getCurrentUser();
-                    if (user != null) {
-                        // User is signed in
-
-                    } else {
+            Intent intent = new Intent(MainActivity.this, profil.class);
+            startActivity(intent);
 
 
-                    }
-                    // ...
-                }
-            };
-
-            mAuth.signInWithEmailAndPassword(email, password1)
-                    .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            SaveData();
-                            // If sign in fails, display a message to the user. If sign in succeeds
-                            // the auth state listener will be notified and logic to handle the
-                            // signed in user can be handled in the listener.
-                            if (!task.isSuccessful()) {
-                                dial();
-                                Toast.makeText(MainActivity.this, R.string.auth_failed,
-                                        Toast.LENGTH_SHORT).show();
-                            }
-
-                            // ...
-                        }
-                    });}
-        Intent intent = new Intent(this, profil.class);
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/dap/whenwash/profil.java
+++ app/src/main/java/com/example/dap/whenwash/profil.java
@@ -3,14 +3,27 @@ package com.example.dap.whenwash;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import static com.example.dap.whenwash.MainActivity.APP_PREFERENCES;
 import static com.example.dap.whenwash.MainActivity.APP_PREFERENCES_EMAIL;
@@ -19,7 +32,12 @@ import static com.example.dap.whenwash.R.layout.abc_alert_dialog_button_bar_mate
 import static com.example.dap.whenwash.R.layout.item;
 
 public class profil extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     private static Context context;
+    private String email="";
+    private String password1="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,10 +45,101 @@ public class profil extends AppCompatActivity {
         setContentView(R.layout.activity_profil);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                } else {
+                }
+                // ...
+            }
+        };
+        dial();
 
 
     }
 
+    public void SaveData(){
+        SharedPreferences values = getApplicationContext()
+                .getSharedPreferences(APP_PREFERENCES, 0);
+        SharedPreferences.Editor editor = values.edit();
+        editor.putString(APP_PREFERENCES_EMAIL,email);
+        editor.putString(APP_PREFERENCES_PASSWORD,password1);
+        editor.commit();
+    }
+
+    public void dial(){
+        LayoutInflater li = LayoutInflater.from(this);
+        View promptsView = li.inflate(R.layout.auth, null);
+        AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(this);
+        mDialogBuilder.setView(promptsView);
+        final EditText userInput = (EditText) promptsView.findViewById(R.id.input_text);
+        final EditText userInput1 = (EditText) promptsView.findViewById(R.id.input_text1);
+
+        mDialogBuilder.setCancelable(false).setPositiveButton("OK",null).setNegativeButton("Назад",null).setNeutralButton("Добавить",null);
+
+        final AlertDialog dialog = mDialogBuilder.create();
+        dialog.show();
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = userInput.getText().toString();
+                password1 = userInput1.getText().toString();
+                if(!email.isEmpty()||!password1.isEmpty()) {
+
+                    mAuth.signInWithEmailAndPassword(email, password1)
+                            .addOnCompleteListener(profil.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        SaveData();
+                                        dialog.dismiss();
+                                    }else{
+                                        Toast.makeText(profil.this, R.string.auth_failed,
+                                        Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                    });
+                }
+            }
+
+        });
+        dialog.getButton(DialogInterface.BUTTON_NEGATIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+        dialog.getButton(DialogInterface.BUTTON_NEUTRAL).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = userInput.getText().toString();
+                password1 = userInput1.getText().toString();
+                if(!email.isEmpty()||!password1.isEmpty()) {
+                mAuth.createUserWithEmailAndPassword(email, password1)
+                        .addOnCompleteListener(profil.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    SaveData();
+                                    dialog.dismiss();
+                                }else{
+                                    Toast.makeText(profil.this, task.getException().toString(),
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+            }}
+        });
+
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -67,4 +176,6 @@ public class profil extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+
 }
